c0017e518c3cbf06b19c80ab4d0a7b0d8593b8ed
BluesOctopus01 <metens.thomas@outlook.be>
2025-12-28 17:35:03 +0100

    Automatisation webhook

--- app/main.py
+++ app/main.py
@@ -1,15 +1,17 @@
-# main.py
+import asyncio
 from contextlib import asynccontextmanager
 from dotenv import load_dotenv
 import os
 
+from fastapi.responses import PlainTextResponse
 import httpx
 import ngrok
 import uvicorn
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, Query
 from loguru import logger
 
-
+#=============Configuration==============
+#chargement de variable d'env
 load_dotenv()
 #info pour Ngrok
 NGROK_AUTH_TOKEN = os.getenv("NGROK_AUTHTOKEN", "")
@@ -18,6 +20,9 @@ APPLICATION_PORT = 5000
 CLIENT_ID =  os.getenv("CLIENT_ID", "")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET", "")
 VERIFY_TOKEN = os.getenv("TOKEN_SECRET", "")
+STRAVA_BASE_URL = "https://www.strava.com/api/v3/push_subscriptions"
+
+#=============Lifespan==============
 
 @asynccontextmanager
 #FastAPI utilise lifespan pour gérer le cycle de vie de l'application
@@ -35,12 +40,26 @@ async def lifespan(app: FastAPI):
 
     # Stocker dans FastAPI pour y accéder plus tard
     app.state.ngrok_url = public_url
+    # --- Création webhook Strava ---
+    #obliger de créer le callback_url dynamiquement
+    callback_url = public_url + "/webhook"
+
+    data = {
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+        "callback_url": callback_url,
+        "verify_token": VERIFY_TOKEN
+    }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post(STRAVA_BASE_URL, json=data)
+        logger.info(f"Strava webhook creation response: {response.status_code} | {response.text}")
 
     yield
     #teardown()
     logger.info("Tearing Down ngrok Endpoint")
     await listener.close()
-
+#App
 app = FastAPI(lifespan=lifespan)
 
 
@@ -50,37 +69,38 @@ app = FastAPI(lifespan=lifespan)
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/ngrok")
+@app.get("/ngrok/info")
 async def get_ngrok_url():
-    return {"ngrok_url": app.state.ngrok_url}
+    ngrok_url = getattr(app.state, "ngrok_url", None)
+    if ngrok_url:
+        return {"ngrok_url": ngrok_url}
+    else:
+        return {"ngrok_url": "url Error"}
 
-#todo continuer et lire la doc
 @app.get("/webhook")
-async def webhook_verification():
-    return {"message": "Webhook OK"}
-#TODO Continuer a comprendre comment fonctionne l'asynchrone
-@app.post("/strava")
-async def test():
-    #comme le callback_url n'est pas fixe du a la version gratuire de Ngrok ont le défini dynamiquement
-    callback_url = app.state.ngrok_url + "/webhook"
-    data = {
-        "client_id": CLIENT_ID,
-        "client_secret": CLIENT_SECRET,
-        "callback_url": callback_url,
-        "verify_token": VERIFY_TOKEN
-    }
-    async with httpx.AsyncClient() as client:
-        response = await client.post(
-            "https://www.strava.com/api/v3/push_subscriptions",
-            data=data
-        )
-        print("HTTP status:", response.status_code)
-        print("Response content:", response.text)
+async def webhook_verification(
+    hub_mode: str = Query(..., alias="hub.mode"),
+    hub_challenge: str = Query(..., alias="hub.challenge"),
+    hub_verify_token: str = Query(..., alias="hub.verify_token")
+):
+    logger.info(f"Webhook GET verification called with: mode={hub_mode}, challenge={hub_challenge}, verify_token={hub_verify_token}")
+    
+    if hub_verify_token != VERIFY_TOKEN:
+        return {"error": "Invalid verify token"}
+    
+    return PlainTextResponse(content=hub_challenge)
 
-    return {
-        "status_code": response.status_code,
-        "response": response.text
-    }
+# Endpoint POST pour recevoir les événements Strava
+@app.post("/webhook")
+async def webhook_events(payload: dict):
+    owner_id = payload.get("owner_id","unkown")
+    object_type = payload.get("object_type","unkown")
+    aspect_type = payload.get("aspect_type","unkown")
+
+    logger.info(f"Webhook event received | athlete={owner_id} type={object_type} action={aspect_type}")
+
+    return {"status": "ok"}
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=APPLICATION_PORT, reload=True)
